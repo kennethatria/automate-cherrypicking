fdaf7f8e2b2b03c89e8ba840a76257e9d2511553
kennethatria <kennethatria@gmail.com>
2021-07-30 17:49:40 +0300

    v0.0.1: finding the commit

--- main.go
+++ main.go
@@ -5,25 +5,59 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
+var count = 0
+var commit_array_size [15]string
+var provided_args []string
+
 func main() {
-	count := 0
-	data, err := os.Open("logs.txt")
+	provided_args := os.Args[1:]
+	automater_mode(provided_args)
+}
+
+func automater_mode(provided_args []string) {
+	if "find" == provided_args[0] {
+		find_commit_order(provided_args[1], provided_args[2])
+	} else if "merge" == provided_args[0] {
+		git_cherry_automater(provided_args[1])
+	}
+
+}
+func git_cherry_automater(provided_commit string) {
+	fmt.Println(" merge " + provided_commit)
+}
+
+func find_commit_order(commit_id, file_name string) {
+	fmt.Println("  ############# Commit order############# ")
+	fmt.Println(file_name)
+	data, err := os.Open(file_name)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer data.Close()
-	scanner := bufio.NewScanner(data)
-	for scanner.Scan() {
-		count++
-		fmt.Println(scanner.Text())
-	}
 
-	fmt.Printf("number of commits %d\n provided arguments\n", count)
+	scanner := bufio.NewScanner(data)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	for scanner.Scan() {
+		//find_commit(scanner.Text())
+		s := strings.Split(scanner.Text(), " ")
+		if s[0] == commit_id {
+			fmt.Println(s[0], "bingo !!!, postion:", count)
+		} else {
+			fmt.Println(s[0])
+		}
+
+		commit_array_size[count] = scanner.Text()
+		count++
+
+		if count > 14 {
+			break
+		}
+	}
+	fmt.Println("  ############# End ############# ")
 
 }
